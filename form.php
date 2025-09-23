356f4194a125ecba8e04628f7e7bd1d62ec229c7
Iconic-Developer <aniteshc0@gmail.com>
2025-09-23 20:21:28 +0530

    data inserted

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php
+include ('connection.php');
+
+
+if(isset($_POST['submit'])){
+
+$firstName = $_POST['fname'];
+$lastName = $_POST['lname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$address = $_POST['address'];
+  $gender = null;
+    if(isset($_POST['gender'])){
+        $gender=$_POST['gender'];
+    }
+$city = $_POST['city'];
+
+ $state = null;
+    if(isset($_POST['state'])){
+        $state=$_POST['state'];
+    }
+
+$link = mysqli_query ($conn, "insert into user (ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, ADDRESS, GENDER, CITY, STATE) values (NULL, '$firstName', '$lastName', '$email', '$password', '$address', '$gender', '$city', '$state')");
+
+
+};
